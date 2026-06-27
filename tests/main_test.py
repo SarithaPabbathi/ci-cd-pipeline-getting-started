f57f396934789369cd901d60c68ce9d0539036e5
SarithaPabbathi <saritapabbathi@gmail.com>
2026-06-26 23:43:47 -0400

    Added one test

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,7 +1,13 @@
-from src.main import add
+from src.main import add, subtract
 
 def test_add_function():
     assert add(1,2) == 3
     assert add(4, 2) == 6
     assert add(0, 0) == 0
-    
+
+
+def test_subtract_function():
+    assert subtract(1, 2) == -1
+    assert subtract(4, 2) == 2
+    assert subtract(0, 0) == 0
+
